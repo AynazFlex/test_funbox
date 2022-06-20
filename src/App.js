101e648af673c4514a52708a48247d897fbb9230
AynazFlex <jkz1713x2@gmail.com>
2022-06-20 20:21:23 +0300

    fix start

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div className={style.wrapper}>
-      <div className={style.container}>
-        <h1 className={style.title}>Ты сегодня покормил кота?</h1>
-        <div className={style.cards}>
-          {state.cards.map((card) => (
-            <Card
-              key={card.id}
-              {...card}
-              dispatch={dispatch}
-              setSelect={setSelect}
-            />
-          ))}
-        </div>
+    <div className={style.container}>
+      <h1 className={style.title}>Ты сегодня покормил кота?</h1>
+      <div className={style.cards}>
+        {state.cards.map((card) => (
+          <Card
+            key={card.id}
+            {...card}
+            dispatch={dispatch}
+            setSelect={setSelect}
+          />
+        ))}
       </div>
     </div>
   );
